f81ca71d3d36645f0fa924aca6cbeb4359cb1397
Sandeep Kumar <sandeepkumar0486@gmail.com>
2017-08-28 15:15:13 +0530

    signin and signup api issue resolved

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,38 +1,31 @@
-class Api::v1::SessionsController < Devise::BaseController
-  prepend_before_filter :require_no_authentication, :only => [:create ]
-  include Devise::Controllers::InternalHelpers
-  
-  before_filter :ensure_params_exist
+class Api::V1::SessionsController < Devise::SessionsController
+	prepend_before_action :verify_user, only: [:destroy]
+	skip_before_action :verify_authenticity_token
+	respond_to :json
 
-  respond_to :json
-  
-  def create
-    build_resource
-    resource = User.find_for_database_authentication(:login=>params[:user_login][:login])
-    if resource.nil?
-      render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
-    end
+	def create
+		resource = User.find_for_database_authentication(:email=>params[:email])
+		if resource.nil?
+			render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+		end
 
-    if resource.valid_password?(params[:user_login][:password])
-      sign_in("user", resource)
-      render :json=> {:success=>true, :auth_token=>resource.authentication_token, :login=>resource.login, :email=>resource.email}, :status=>201
-    else
-      render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
-    end
-  end
-  
-  def destroy
-    sign_out(resource_name)
-  end
+		if resource.valid_password?(params[:password])
+			sign_in("user", resource)
+			render :json=> {:success=>true, :auth_token=>resource.auth_token,  :email=>resource.email}, :status=>201
+		else
+			render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+		end
+	end
 
-  protected
-  def ensure_params_exist
-    return unless params[:user_login].blank?
-    render :json=>{:success=>false, :message=>"missing user_login parameter"}, :status=>422
-  end
+	def destroy
+		sign_out(resource_name)
+	end
 
-  def invalid_login_attempt
-    warden.custom_failure!
-    render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
-  end
+	protected
+
+
+	def invalid_login_attempt
+		warden.custom_failure!
+		render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  root 'home#index'
+	root 'home#index'
 
- #  namespace :api do
- #  	namespace :v1 do
+	#  namespace :api do
+	#  	namespace :v1 do
 	#   	devise_for :users, controllers: { sessions: "sessions", registrations: "registrations" }
 	#   end
-	# end  
+	# end
 
 	namespace :api, defaults: {format: 'json'} do
-    namespace :v1 do
-      resources :users
-      devise_for :users, path: '', path_names: {sign_in: "login", sign_out: "logout"},
-                                      controllers: {registrations: "registrations", sessions: "sessions"}
-    end
-  end
+		namespace :v1 do
+			# resources :users
+			devise_for :users,  path_names: {sign_in: "login", sign_out: "logout"},
+				controllers: {registrations: "api/v1/registrations", sessions: "api/v1/sessions"}
+		end
+	end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
