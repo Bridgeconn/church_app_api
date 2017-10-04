134670f4c7d0e252ec77a6d4fa5403ded78df7fc
udkumar <udkumar@hotmail.com>
2017-10-04 16:08:37 +0530

    rolify added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,6 +8,24 @@ class User < ApplicationRecord
   require 'securerandom'
   before_create :set_auth_token
 
+  # after_create :send_admin_mail
+  # def send_admin_mail
+  #   AdminMailer.new_user_waiting_for_approval(self).deliver
+  # end
+
+  # def active_for_authentication? 
+  #   super && approved? 
+  # end 
+  
+  # def inactive_message 
+  #   if !approved? 
+  #     :not_approved 
+  #   else 
+  #     super # Use whatever other message 
+  #   end 
+  # end
+
+  
   private
   def set_auth_token
     return if auth_token.present?
