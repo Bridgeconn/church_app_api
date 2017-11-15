284fd23fc49588d7603ac1cd5961d623021660a5
umitoit <udayonrails@gmail.com>
2017-11-15 16:59:05 +0530

    changes relation and added church app id filter

--- app/models/church_app.rb
+++ app/models/church_app.rb
@@ -3,4 +3,10 @@ class ChurchApp < ApplicationRecord
 	has_many :events
 
 	has_many :members, :class_name => 'User'
+
+	before_create :church_id_generate
+
+	def church_id_generate
+    self.church_app_id = "ChurchApp_" + (0...5).map { ('1'..'10').to_a[rand(4)] }.join
+  end
 end
