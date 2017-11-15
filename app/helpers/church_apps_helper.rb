0883a56068211435ae43b7862de842827d8fb8c5
umitoit <udayonrails@gmail.com>
2017-11-15 16:59:05 +0530

    added full address in helper

--- app/helpers/church_apps_helper.rb
+++ app/helpers/church_apps_helper.rb
@@ -1,11 +1,21 @@
 module ChurchAppsHelper
-	def fullname(id)
-		user = User.find(id)
+	def fullname(user_id)
+		user = User.find(user_id)
 		if user
 			name = "#{user.first_name} #{user.last_name}"
 			return name
 		else
 			name = ""
+		end
+	end
+
+	def address_combine(church_id)
+		church = ChurchApp.find(church_id)
+		if church
+			address = "#{church.address1}, #{church.address3}"
+			return address
+		else
+			address = ""
 		end
 	end
 end
