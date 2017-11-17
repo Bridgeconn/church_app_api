04e7e33e0e744482379d9c13ee506e8552eaa9b9
umitoit <udayonrails@gmail.com>
2017-11-17 14:06:17 +0530

    updated sighup API for chuch app

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,13 +3,24 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 	respond_to :json
 
 	def create
-		user = User.new(user_params)
-		if user.save
-			render :json=> user.as_json(:auth_token=>user.auth_token, :email=>user.email), :status=>201
-			return
-		else
-			warden.custom_failure!
-			render :json=> user.errors, :status=>422
+		if request.headers["Church-App-Id"].present?
+			@churchId = request.headers["Church-App-Id"]
+			@churchApp = ChurchApp.find_by_church_app_id("#{@churchId}")
+			if @churchApp.present?
+				@admin_user = @churchApp.user
+				user = User.new(user_params)
+				user.add_role :member
+				user.member_belongs_to_admin = @admin_user.id
+				if user.save
+					render :json=> {:auth_token=>user.auth_token, :email=>user.email, :church=>@churchApp, :admin=>@admin_user, :status=>201}
+					return
+				else
+					warden.custom_failure!
+					render :json=> user.errors, :status=>422
+				end
+			else
+				render :json=> {:invalid=>"Church App Invalid.", :suggestion=>"Contact to Church Owner."}, :status=>208
+			end
 		end
 	end
 
