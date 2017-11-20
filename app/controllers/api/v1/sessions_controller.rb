d67053c5df1c34fabd7eff3ba95d11b221098b01
umitoit <udayonrails@gmail.com>
2017-11-20 15:06:44 +0530

    updated APIs

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,22 +1,31 @@
 class Api::V1::SessionsController < Devise::SessionsController
 	prepend_before_action :verify_user, only: [:destroy]
 	skip_before_action :verify_authenticity_token
+	respond_to :json
 
 	def create
-		resource = User.find_for_database_authentication(:email=>params[:email])
-		if resource.nil?
-			render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
-		end
-
-		if resource.valid_password?(params[:password])	
-			sign_in("user", resource)
-			if resource.has_role? :admin
-				render :json=> {:success=>true, :auth_token=>resource.auth_token,  :email=>resource.email, :user_type=>"admin"}, :status=>201
+		if request.headers["Church-App-Id"].present?
+			@churchId = request.headers["Church-App-Id"]
+			@churchApp = ChurchApp.find_by_church_app_id("#{@churchId}")
+			resource = User.find_for_database_authentication(:email=>params[:email])
+			if resource.nil?
+				render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+			end
+			if resource.valid_password?(params[:password])	
+				if resource.approved == true
+					sign_in("user", resource)
+					if resource.has_role? :member
+						@admin_user = @churchApp.user
+						render :json=> {:success=>true, :user => {:auth_token=>resource.auth_token,  :email=>resource.email, :first_name => resource.first_name, :last_name => resource.last_name, :user_status =>resource.approved, :user_contact =>resource.contact_number}, :admin_email=> @admin_user.email, :church_app_id => @churchApp.church_app_id, :church_name => @churchApp.name, :church_address => "#{@churchApp.address1 + " " + @churchApp.address3}", :status=>201}
+					end
+				else
+					render :json=> {:success=>false, :message=>"Account is Not Approved Yet! Please Contact to Church Admin."}, :status=>208
+				end
 			else
-				render :json=> {:success=>true, :auth_token=>resource.auth_token,  :email=>resource.email}, :status=>201
+				render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
 			end
 		else
-			render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+			render :json=> {:invalid=>"Church App Invalid.", :suggestion=>"Contact to Church Owner."}, :status=>208
 		end
 	end
 
