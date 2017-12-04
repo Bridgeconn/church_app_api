d6f0af13e6cfc32c04cc3f4a444f043401dfbab7
umitoit <udayonrails@gmail.com>
2017-12-04 12:31:07 +0530

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(password: "12345678", email: "admin@test.com", password_confirmation: "12345678")
-user1.add_role :admin
 
-user2 = User.create(password: "12345678", email: "super_admin@test.com", password_confirmation: "12345678")
+user2 = User.create(email: "super_admin@test.com", password: "12345678", password_confirmation: "12345678")
 user2.add_role :super_admin
+
+user1 = User.create(password: "12345678", email: "admin@test.com", password_confirmation: "12345678", member_belongs_to_admin: user2.id)
+user1.add_role :admin
 
 
